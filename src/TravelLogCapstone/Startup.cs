428b7405e664fba9e04ee5a8c895e02e0071dccd
eorstrom <eorstrom2010@gmail.com>
2016-06-22 22:50:29 -0500

    successfully able to POST to Restaurants table from input fields in front-end code.

--- src/TravelLogCapstone/Startup.cs
+++ src/TravelLogCapstone/Startup.cs
@@ -44,8 +44,8 @@ namespace TravelLogCapstone
                         .AllowAnyMethod()
                         .AllowAnyHeader());
                 //.WithMethods("DELETE, PUT, POST, GET, OPTIONS"));
-            });
-        }
+                });
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
